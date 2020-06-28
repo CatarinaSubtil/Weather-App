831d4b7fc035d7705db80f780a904b6a370c8a68
CatarinaSubtil <65078979+CatarinaSubtil@users.noreply.github.com>
2020-06-28 19:13:59 +0100

    Updated Time and introduced Api Weather

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // Format Date and Time
-function formatDateTime(date) {
+function formatDateTime(timestamp) {
+    let now = new Date();
+
     let weekDay = [
         "Sunday",
         "Monday",
@@ -39,8 +41,31 @@ function formatDateTime(date) {
     ${now.getDate()} ${month[now.getMonth()]} ${now.getFullYear()} ${hour}:${minutes}`;
 }
 
-let newDate = document.querySelector("#date");
-let now = new Date();
-newDate.innerHTML = formatDateTime(now);
+//City Weather Details
+function displayWeatherDetails(response) {
+    let city = document.querySelector("#location");
+    city.innerHTML = response.data.name;
 
+    let temp = document.querySelector("#weather-temperature");
+    temp.innerHTML = `${Math.round(response.data.main.temp)}°`;
 
+    let description = document.querySelector("#weather-description");
+    description.innerHTML = response.data.weather[0].description;
+
+    let feelsLikeTemp = document.querySelector("#feels-like");
+    feelsLikeTemp.innerHTML = `${Math.round(response.data.main.feels_like)} °`;
+
+    let wind = document.querySelector("#wind");
+    wind.innerHTML = response.data.wind.speed;
+
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = response.data.main.humidity;
+
+    let date = document.querySelector("#date");
+    date.innerHTML = formatDateTime(response.data.dt * 1000);
+}
+
+let apiKey = "248526705cf1e69e1604c72809dd3b61";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayWeatherDetails);
